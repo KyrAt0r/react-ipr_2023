5e0d2e4d744dc690be6c57a0b5116fbb5b2c45e2
Latypov.vd <Latypov.VD@parmalogica.ru>
2023-02-22 17:42:20 +0500

    -

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const element = (
+    <div>
+        <h1 className={'hello-text'}>Hello, world!</h1>
+        <h2>It is {new Date().getDate()}.</h2>
+    </div>
+);
+
+
+root.render(element);
+
+
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
